46d0ae7f82cf878bfe37cccd692dfb6831a363c0
stutton <stephen.tutton@gmail.com>
2018-01-06 16:55:01 -0500

    Initial Upload

---
+++ ExampleWpf/ExampleWpf/NotifyBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWpf
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T oldValue, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(oldValue, newValue)) return;
+            oldValue = newValue;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ ExampleWpf/ExampleWpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ExampleWpf.ViewModel
+{
+    public class MainViewModel : NotifyBase
+    {
+        
+        public MainViewModel()
+        {
+            _submitCommand = new RelayCommand(_ => MessageBox.Show("Success"));
+        }
+
+        private ICommand _submitCommand;
+
+        public ICommand SubmitCommand
+        {
+            get => _submitCommand;
+            set => _submitCommand = value;
+        }
+
+        private string _userName;
+
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+
+        private string _apiKey;
+        
+        public string ApiKey
+        {
+            get => _apiKey;
+            set => SetProperty(ref _apiKey, value);
+        }
+    }
+}
